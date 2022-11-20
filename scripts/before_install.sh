6103b0884071ec054236dd37e7cb258cb7e0507a
Ruben Cipriano <i170005@inete.net>
2022-11-20 13:29:07 +0000

    Before Install NVM

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-
 DIR="/home/ec2-user/watch-together"
 if [ -d "$DIR" ]; then
     echo "${DIR} exists"
